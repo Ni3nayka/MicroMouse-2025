385f2e466f9929fa4255e646fa7a2c1e4cabeee2
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2025-03-12 21:51:50 +0300

    main code structure

---
+++ src/robot/ExtraMazeSolver.h
@@ -0,0 +1,17 @@
+/*
+  code write for project:
+  https://github.com/Ni3nayka/MicroMouse-2025
+
+  author: Egor Bakay <egor_bakay@inbox.ru> Ni3nayka
+  write:  March 2025
+  modify: March 2025
+*/
+
+#pragma once
+
+class ExtraMazeSolver {
+  public:
+    // ExtraMazeSolver() {}
+    // void setup() {}
+  private:
+};
---
+++ src/robot/robot.ino
@@ -0,0 +1,25 @@
+/*
+  code write for project:
+  https://github.com/Ni3nayka/MicroMouse-2025
+
+  author: Egor Bakay <egor_bakay@inbox.ru> Ni3nayka
+  write:  March 2025
+  modify: March 2025
+*/
+
+#include <arduino_encoder.h>
+// #include <AVOCADO_esp.h>
+#include <easy_arduino_navigator.h>
+#include <HNR-252_DCv0_1.h>
+#include "ExtraMazeSolver.h"
+#include "lazer.h"
+
+void setup(){
+  Serial.begin(115200);
+}
+
+void loop() { 
+  // esp.update(); 
+}
+
+
