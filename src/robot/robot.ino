f7090f4cf7045b5a6b29abeda74390cf5a8c435b
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2025-03-12 23:03:48 +0300

    Update robot.ino

--- src/robot/robot.ino
+++ src/robot/robot.ino
@@ -13,13 +13,24 @@
 #include <HNR-252_DCv0_1.h>
 #include "ExtraMazeSolver.h"
 #include "lazer.h"
+Lazer lazer_1(0x30,15);
+Lazer lazer_2(0x31,16);
+Lazer lazer_3(0x32,17);
+Lazer lazer_4(0x33,5);
+// Lazer lazer_2(0x34,19);
 
 void setup(){
   Serial.begin(115200);
+  firstSetupLazers();
+  lazer_1.setup();
+  lazer_2.setup();
+  lazer_3.setup();
+  lazer_4.setup();
 }
 
 void loop() { 
-  // esp.update(); 
+  Serial.println(String(lazer_1.get()) + " " + String(lazer_2.get()) + " " + String(lazer_3.get()) + " " + String(lazer_4.get()));
+  delay(100);
 }
 
 
