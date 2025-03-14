4901f81fc66c25f73a7d4df1e59bbf5284782007
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2025-03-14 15:47:15 +0300

    Create pins.h

---
+++ src/robot/pins.h
@@ -0,0 +1,27 @@
+/*
+  code write for project:
+  https://github.com/Ni3nayka/MicroMouse-2025
+
+  author: Egor Bakay <egor_bakay@inbox.ru> Ni3nayka
+  write:  March 2025
+  modify: March 2025
+*/
+
+#pragma once
+
+#define LASER_PIN_1 15
+#define LASER_PIN_2 16
+#define LASER_PIN_3 17
+#define LASER_PIN_4 5
+#define LASER_PIN_5 32
+
+#define LASER_ADDRESS_1 0x30
+#define LASER_ADDRESS_2 0x31
+#define LASER_ADDRESS_3 0x32
+#define LASER_ADDRESS_4 0x33
+#define LASER_ADDRESS_5 0x34
+
+#define ENC1_PIN_1 34
+#define ENC1_PIN_2 35
+#define ENC2_PIN_1 39
+#define ENC2_PIN_2 36
